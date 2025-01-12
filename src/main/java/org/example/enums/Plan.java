98affe52a952517c415ccb36de737d2aa5235efd
berk-karademir <berk.karademir@hotmail.com>
2025-01-12 14:10:52 +0300

    Plan enum updated, new instances created

--- src/main/java/org/example/enums/Plan.java
+++ src/main/java/org/example/enums/Plan.java
@@ -1,26 +1,28 @@
 package org.example.enums;
 
 public enum Plan {
-    BASIC("basic", 1000),
-    MIDDLE("middle", 1500);
-
+    BASIC("Basic Plan", 100.0),
+    PREMIUM("Premium Plan", 250.0),
+    ENTERPRISE("Enterprise Plan", 500.0);
 
     private final String name;
-    private final int price;
+    private final double price;
 
-    //constructor
-    Plan(String name, int price) {
+    Plan(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-
-    //getters
     public String getName() {
         return name;
     }
 
-    public int getPrice() {
+    public double getPrice() {
         return price;
+    }
+
+    @Override
+    public String toString() {
+        return name + " ($" + price + ")";
     }
 }
